4ae25474f09fe490801114396764926f3f836cc4
Simao Nziaka <sdnziaka@gmail.com>
2018-01-20 16:26:48 -0700

    Get the name of a number from a string

--- Demo.java
+++ Demo.java
@@ -9,6 +9,10 @@ public class Demo {
 		System.out.print("Insert a number: ");
 		Integer num = input.nextInt();
 
+		//Pass an Integer
 		System.out.print("The number is: " + Numname.read(num) + "\n");
+		//Pass a string
+		String strnum = "33";
+		System.out.print("The number is: " + Numname.read(strnum) + "\n");
 	}
 }
